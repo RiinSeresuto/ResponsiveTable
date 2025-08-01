3f2a6f60608bd9c84ac82f65a1ae76d81789184c
RiinSeresuto <renebaribal@gmail.com>
2025-08-01 10:54:29 +0800

    clean up

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import path from "path";
 export default defineConfig({
 	build: {
 		rollupOptions: {
-			input: path.resolve(__dirname, "src/main.js"), // Your main entry point
+			input: path.resolve(__dirname, "src/main.js"),
 			output: {
 				entryFileNames: "js/app.bundle.js",
 				assetFileNames: (assetInfo) => {
 					if (assetInfo.name && assetInfo.name.endsWith(".css")) {
-						return "css/app.styles.css"; // Move CSS to /css
+						return "css/app.styles.css";
 					}
 					return "[name][extname]";
 				},
